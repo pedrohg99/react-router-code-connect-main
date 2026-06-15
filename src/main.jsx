bf84d0f70b37d78753c03221eccffc8d6b447ae8
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-15 15:39:31 -0300

    Instala o React Router

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { Login } from './pages/Login/index.jsx'
 import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    {/* <Register /> */}
-    {/* <Feed /> */}
-    {/* <BlogPost /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/register' element={<Register />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/' element={<Feed />} />
+        <Route path='/blog-post' element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
