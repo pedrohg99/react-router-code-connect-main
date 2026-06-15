8d1fe8556b827500bc37b5d4e21ae12c6939aafa
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-15 15:50:34 -0300

    Agrupa por contexto as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,14 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/' element={<Feed />} />
-        <Route path='/blog-post' element={<BlogPost />} />
+        <Route path='/auth'>
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Route>
+        <Route path='/'>
+          <Route path='' element={<Feed />} />
+          <Route path='blog-post' element={<BlogPost />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
