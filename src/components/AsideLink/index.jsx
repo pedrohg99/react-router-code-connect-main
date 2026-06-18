53825bd8481d7cae5a5db6a0920fac2987bb1aaa
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-18 12:18:25 -0300

    Incorpora o componente link dentro do AsideLink

--- src/components/AsideLink/index.jsx
+++ src/components/AsideLink/index.jsx
@@ -1,9 +1,10 @@
 import styles from './asidelink.module.css'
+import { Link } from "react-router-dom";
 
 const AsideLink = ({ href, children }) => {
-    return (<a href={href} className={styles.asidelink}>
+    return (<Link to={href} className={styles.asidelink}>
         {children}
-    </a>)
+    </Link>)
 }
 
 export default AsideLink
